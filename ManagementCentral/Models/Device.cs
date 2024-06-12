020733861eb3fb169c4fce32e8f8329675d8bfef
Mattias Hammarhorn <mattias@hammarhorn.se>
2024-06-12 21:10:59 +0200

    Added Device and DeviceDataService classes, and DeviceOverview razor component

---
+++ ManagementCentral/Models/Device.cs
@@ -0,0 +1,13 @@
+﻿namespace ManagementCentral.Models
+{
+    public class Device
+    {
+        //  namn, id (GUID), aktuell status (online/offline) samt senaste data som skickats från maskinen
+        public Guid Id { get; set; }
+        public string Type { get; set; }
+        public string Location { get; set; }
+        public DateTime? LastEditedOn { get; set; }
+        public string LastData { get; set; }
+        public bool isActive { get; set; }
+    }
+}
---
+++ ManagementCentral/Services/DeviceDataService.cs
@@ -0,0 +1,58 @@
+﻿using ManagementCentral.Models;
+
+namespace ManagementCentral.Services
+{
+    public class DeviceDataService
+    {
+        List<Device> devices = new List<Device>
+        {
+            new Device
+            {
+                Id = Guid.NewGuid(),
+                Location = "Jordbro",
+                isActive = true,
+                Type = "Bulldozer",
+                LastEditedOn = new DateTime(2024,5,29),
+                LastData = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+            },
+            new Device
+            {
+                Id = Guid.NewGuid(),
+                Location = "Farsta",
+                isActive = true,
+                Type = "Lift",
+                LastEditedOn = new DateTime(2024,6,3),
+                LastData = "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            },
+            new Device
+            {
+                Id = Guid.NewGuid(),
+                Location = "Länna",
+                isActive = false,
+                Type = "Excavator",
+                LastEditedOn = new DateTime(2024,6,12),
+                LastData = "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+            }
+        };
+
+        public IEnumerable<Device> GetDevices()
+        {
+            return devices.ToList();
+        }
+
+        public int GetDevicesCount()
+        {
+            return devices.Count();
+        }
+
+        public IEnumerable<Device> GetDevicesOnline()
+        {
+            return devices.Where(d => d.isActive).ToList();
+        }
+
+        public Device GetLastEditedDevice()
+        {
+            return devices.OrderBy(d => d.LastEditedOn).SingleOrDefault();
+        }
+    }
+}
